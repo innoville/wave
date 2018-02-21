15ee4de0de18497dd47100baab6292a1f559608f
EC2 Default User <ec2-user@ip-172-31-23-5.ec2.internal>
2018-02-21 02:31:38 +0000

    + config.json m test.js

--- test.js
+++ test.js
@@ -1,16 +1,15 @@
 var aws = require('aws-sdk');
+aws.config.loadFromPath('./config.json');
 var trans = new aws.TranscribeService({apiVersion: '2017-10-26'});
 var params = {
   LanguageCode: "en-US",
   Media: {
-    MediaFileUri: "./test.mp4" 
+    MediaFileUri: "https://s3.amazonaws.com/video-src-repo/test.mp4" 
   },
   MediaFormat: "mp4",
-  TranscriptionJobName: 'stt test',
-  MediaSampleRateHertz: 0 
+  TranscriptionJobName: 'stt-test'
+  //MediaSampleRateHertz: 0 
 };
-
-aws.config.region_name = "us-east-1";
 
 trans.startTranscriptionJob(params, function (err, data) {
   if (err) console.log(err, err.stack);
